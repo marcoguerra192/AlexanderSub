f4533a0d7614b7009972671149071c65114560fa
Marco Guerra <marco.guerra192@gmail.com>
2026-04-24 15:16:43 +0200

    Simplicial complex class

---
+++ src/simplicial_complex.py
@@ -0,0 +1,160 @@
+''' Simplicial complex class
+
+The contract is as follows:
+
+1.
+    The simplex list must be closed by subsets. The class does not accept
+    top-dimensional representations. If not enforced, it will crash.
+
+2.
+    0-simplices in simplices are immaterial: points determines the existence
+    of 0-simplices: if a point is never referenced in simplices, it will 
+    nonetheless be included, and the ordering will reflect this.  
+
+3.
+    We are assuming that all simplicial complexes can be embedded in the
+    d-disk. Therefore, the dimension of every simplicial complex cannot 
+    exceed the ambient dimension. If any simplex is passed that exceeds
+    the ambient dimension, it will be ignored.
+    Currently, only dimension 2 and 3 are implemented. 
+    
+4. 
+    Simplices are to be passed as a list of sorted lists. This is assumed
+    true and not checked. If not enforced, it can fail or crash. The same
+    is assumed when accessing objects.
+   
+'''
+
+import numpy as np
+from itertools import combinations
+
+class SimplicialComplex:
+
+    def __init__(self, points, simplices ):
+
+        ''' Constructor
+
+        Parameters:
+        points: np.array of floats, shape N x d, with d=1,2 or 3
+        simplices : list of sorted lists.
+        '''
+
+        if not isinstance(points, np.ndarray):
+            raise ValueError("points must be a numpy array")
+
+        if not np.issubdtype(points.dtype, np.floating):
+            raise ValueError("points must be a numpy array of floats")
+
+        self.ambient_dim = points.shape[1]
+        
+        if self.ambient_dim not in [2,3]:
+            raise ValueError("points must be N x d with d=2,3")
+
+        # store the points coordinates -> these fix the vertices positions
+        self.points = points
+        self.n_points = points.shape[0]
+
+        if self.n_points == 0:
+            raise ValueError("Empty complex - error")
+
+        # processing the simplices
+        
+        if not isinstance(simplices, list):
+            raise ValueError("simplices must be a list of sorted lists")
+
+        if not all([ isinstance(l, list) for l in simplices]):
+            raise ValueError("simplices must be a list of sorted lists")
+
+        self._simplices_by_dim = {}
+        self._cumulative = [0]
+
+        # dimension of the complex - increased as simplices are read
+        self.dim = 0
+
+        # treat 0-simplices separately
+        self._simplices_by_dim[0] = np.arange(self.n_points, dtype=np.int32).reshape(-1, 1)
+        self._cumulative.append(self.n_points)
+
+        # CHECK FOR DEBUGGING, but then can remove
+        max_dim = max((len(s) for s in simplices), default=1) - 1
+        assert max_dim <= self.ambient_dim
+
+        # divide by dimension
+        for k in range(1, self.ambient_dim + 1):
+            simp_of_dim_k = [ s for s in simplices if len(s) == k+1 ]
+            if simp_of_dim_k:
+                self._simplices_by_dim[k] = np.array(simp_of_dim_k, dtype=np.int32)
+                self.dim = k
+            else:
+                self._simplices_by_dim[k] = np.empty((0,k+1), dtype=np.int32)
+                
+            self._cumulative.append(self._cumulative[-1] + len(simp_of_dim_k))
+
+
+        ### LOOKUP dictionaries - Check whether it's worth making O(dim)
+        ## instead of O(1), with a large memory saving
+
+        # _flat_dim[i] gives the dimension of the flat index i
+        self._flat_dim = np.concatenate([
+            np.full(len(self._simplices_by_dim[k]), k, dtype=np.int32)
+            for k in sorted(self._simplices_by_dim)
+        ])
+
+        # _flat_within[i] is the index of sigma of flat index i, within 
+        # the list of appropriate dimension
+        self._flat_within = np.concatenate([
+            np.arange(len(self._simplices_by_dim[k]), dtype=np.int32)
+            for k in sorted(self._simplices_by_dim)
+        ])
+
+        # map from a list of vertex indices to flat index
+        self._simplex_to_flat = {}
+        flat = 0
+        for k in sorted(self._simplices_by_dim):
+            for row in self._simplices_by_dim[k]:
+                self._simplex_to_flat[tuple(row)] = flat
+                flat += 1
+
+    @property
+    def simplices_in_dim(self):
+        return _DimView(self)
+    
+    def flat_index_by_simplex(self, simplex):
+        '''
+        simplex is a list of ints, each a vertex.
+        returns the global flat index
+        '''
+        return self._simplex_to_flat[tuple(simplex)]
+        
+    def flat_from_dim_index(self, dim, idx):
+        '''
+        dim is the dimension, idx the index within that dimension
+        returns the global flat index
+        '''
+        return self._cumulative[dim] + idx
+        
+    def simplex_by_flat_index(self, idx):
+        '''
+        idx is the global flat index. 
+        returns the simplex as a list of ints, each a vertex
+        '''
+        dim = self._flat_dim[idx]
+        index = self._flat_within[idx]
+
+        return self._simplices_by_dim[dim][index]
+            
+''' Trick to not actually store the vertices. 
+If asked the 0-simplices, generates an arange on the fly
+Otherwise, returns the appropriate _simplices_by_dim
+from the SimplicialComplex instance 
+'''
+class _DimView:
+    def __init__(self, Complex):
+        self._c = Complex
+        
+    def __getitem__(self, k):
+        if k == 0:
+            return np.arange(self._c.n_points, dtype=np.int32)
+        return self._c._simplices_by_dim[k] 
+
+        
